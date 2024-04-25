dafbeab9f753cb551678b70e1ed6a2d974404d0d
UrmilaKL <urmilaklbhat@gmail.com>
2024-04-25 16:26:05 +0530

    Automation_Framework_modified_Files3

--- script/test_invalid_login.py
+++ script/test_invalid_login.py
@@ -1,9 +1,14 @@
+import pytest
+
 from generic.base_setup import Base_Setup
 from generic.excel import Excel
 from page.login_page import LoginPage
 
 class Test_InValidLogin(Base_Setup):
+    @pytest.mark.run(order=2)
     def test_invalid_login(self):
+        un = Excel.get_cell_data("../test_data/input.xlsx","invalid_login",2, 1)
+        pw = Excel.get_cell_data("../test_data/input.xlsx","invalid_login",2,2)
         # 1. Enter invalid UN
         loginpage = LoginPage(self.driver)
         loginpage.set_username("urmilaklbhat@kkkk.com")
--- script/test_valid_login.py
+++ script/test_valid_login.py
@@ -1,14 +1,20 @@
+import pytest
+
 from generic.base_setup import Base_Setup
 from generic.excel import Excel
 from page.login_page import LoginPage
+from generic.excel import Excel
 from page.enter_time_track_page import EnterTimeTrackPage
 class Test_ValidLogin(Base_Setup):
+    @pytest.mark.run(order=1)
     def test_valid_login(self):
+        un = Excel.get_cell_data("../test_data/input.xlsx","valid_login",2, 1)
+        pw = Excel.get_cell_data("../test_data/input.xlsx","valid_login",2,2)
         # 1. Enter valid UN
         loginpage = LoginPage(self.driver)
-        loginpage.set_username("urmilaklbhat@gmail.com")
+        loginpage.set_username(un)
         # 2. Enter valid PW
-        loginpage.set_password("b4@p8A@b")
+        loginpage.set_password(pw)
         # 3. Click on login button
         loginpage.click_loginButton()
         # 4. Verify the home page is displayed
